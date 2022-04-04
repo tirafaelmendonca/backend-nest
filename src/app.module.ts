7885fbad2788dbffa912eb0794460411c22db275
tirafaelmendonca@gmail.com <tirafaelmendonca@gmail.com>
2022-04-03 22:56:13 -0300

    Removendo arquivos desnecessários

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { LawsuitModule } from './lawsuit/lawsuit.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
@@ -11,11 +9,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     ),
     LawsuitModule,
   ],
-  controllers: [
-    AppController,
-  ],
-  providers: [
-    AppService,
-  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
